a238093b9cf7ac39d1c7ad70bb65e615e5ee0222
codeamac11 <codeamac11@gmail.com>
2017-02-08 17:53:09 -0600

    Creando repositorio y primer parte del codigo de tortas.rb

---
+++ tortas.rb
@@ -0,0 +1,16 @@
+class Tortas
+  attr_accessor :sabor :t_coccion
+  def initialize
+    @sabor
+    
+  end
+  
+end
+
+class Horno
+attr_accessor :crudo, :casi_listo, :listo, :quemado
+  def initialize
+    @crudo 
+    @ca
+  end
+end
